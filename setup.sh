8401d58c8a31534da1a1323c7742997e996495ad
MatildaSif <matildasifrk@gmail.com>
2026-03-18 09:47:00 +0100

    setup and some requirements

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# create virtual env
+python -m venv env
+
+# activate env
+source ./env/bin/activate
+
+# install requirements
+pip install --upgrade pip
+pip install -r requirements.txt
+python -m spacy download en_core_web_sm
+
+
+# close the environment
+deactivate
